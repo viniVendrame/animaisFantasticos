2279b28da64a52a014c39886962e6600b4dc8493
viniVendrame <52543661+viniVendrame@users.noreply.github.com>
2021-04-29 17:51:53 -0300

    Finalizado animação no scroll do stie

--- scrips/app.js
+++ scrips/app.js
@@ -36,10 +36,28 @@ const accordionTab = () =>{
 
 }
 
+const scrollAnimation = () =>{
+    const sections = document.querySelectorAll('.js-scroll')
+    const windowMetade = window.innerHeight * 0.6 
+
+
+    function animaScroll(){
+        sections.forEach((item) =>{
+            const sectionTop = item.getBoundingClientRect().top - windowMetade
+            if(sectionTop < 0){
+                item.classList.add('ativo')
+            }
+        })
+    }
+    animaScroll()
+    window.addEventListener('scroll', animaScroll)
+
+}
 
 const allFunction = () =>{
     tabMenu()
     accordionTab()
+    scrollAnimation()
 }
 
 allFunction()
